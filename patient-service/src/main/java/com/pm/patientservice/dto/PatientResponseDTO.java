9112a2de69c63108af6ef4c106a24fed4f20e99b
Sergio Alfonso <nikerblack@hotmail.com>
2025-04-08 11:53:41 -0500

    Our first CRUD Patient

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,50 @@
+package com.pm.patientservice.dto;
+
+public class PatientResponseDTO {
+
+    private String id;
+    private String name;
+    private String email;
+    private String adress;
+    private String dateOfBirth;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
